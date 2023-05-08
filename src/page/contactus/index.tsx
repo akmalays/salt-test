717d1b4944b6b6459f3bd24dd2ce72251c4aa946
akmalays <akmalays@gmail.com>
2023-05-08 15:47:44 +0700

    add contact us location

--- src/page/contactus/index.tsx
+++ src/page/contactus/index.tsx
@@ -1,11 +1,44 @@
-import { Grid, Typography } from "@mui/material";
+import { useState } from "react";
+
+import { Grid, MenuItem, TextField, Typography } from "@mui/material";
 
 import CompanyWhite from "../../assets/icons/logo-white.svg";
 import SvgIcon from "../../components/icon/Icon";
 
 const contactValue = ["Who We Are", "Our Values", "The Perks"];
+const departmentVal = [
+  {
+    id: 1,
+    departement: "Information and Technology",
+  },
+  {
+    id: 2,
+    departement: "Finance and Collecion",
+  },
+  {
+    id: 3,
+    departement: "People Services",
+  },
+];
 
 export default function ContactUs() {
+  const [deptAddress, setDeptAddress] = useState<string>(
+    "Information and Technology"
+  );
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDeptAddress(event.target.value);
+  };
+  const returnAddressValue = (deptAddress: string) => {
+    switch (deptAddress) {
+      case "Information and Technology":
+        return "Jl. Lembong No 8 Kel. Braga Kec. Sumur Bandung, Kota Bandung, Jawa Barat";
+      case "Finance and Collecion":
+        return "Jl. Madakaripura No 1266 Kel. Braga Kec. Sumur Utara, Kota Bandung, Jawa Barat";
+      case "People Services":
+        return "Jl. Parayangn No 8gA Kel. Braga Kec. Sumur CC, Kota Bandung, Jawa Barat";
+    }
+  };
+
   return (
     <div>
       <Grid sx={{ backgroundColor: "#08385F" }}>
@@ -37,13 +70,47 @@ export default function ContactUs() {
             <Grid
               sx={{
                 backgroundColor: "#ffffff",
-                height: 100,
+                height: 150,
                 width: 800,
                 borderRadius: 2,
+                p: 2,
               }}
-            ></Grid>
+            >
+              <Grid
+                sx={{
+                  display: "grid",
+                  justifyContent: "flex-end",
+                  gap: 2,
+                }}
+              >
+                <TextField
+                  size="small"
+                  sx={{ width: 300 }}
+                  select
+                  type="email"
+                  value={deptAddress}
+                  onChange={handleChange}
+                >
+                  {departmentVal.map((val) => (
+                    <MenuItem key={val.id} value={val.departement}>
+                      {val.departement}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                <Typography
+                  sx={{
+                    fontWeight: 500,
+                    fontSize: 14,
+                    color: "#25A0D8",
+                    maxWidth: 350,
+                  }}
+                >
+                  {returnAddressValue(deptAddress)}
+                </Typography>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid mt={-5}>
+          <Grid mt={-10}>
             {contactValue.map((val) => {
               return (
                 <Typography
